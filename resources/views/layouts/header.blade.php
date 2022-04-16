73bf86ca9b873eba29f613c877759da3f341675a
josh <61042416+joshuafirme@users.noreply.github.com>
2022-04-16 16:30:43 +0800

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8" />
     <title>@yield('title') | Flying High</title>
     <meta content="Dashboard" name="description" />
-    <meta name="csrf-token" content="jtuZZgSJMUGRsEosBxyoHUrmkjJOR1E6CgJIGzpG" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -18,7 +17,7 @@
     <link href="{{ asset('css/bootstrap-datepicker.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/chartist.min.css') }}">
     <link href="{{ asset('css/select2.min.css') }}" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css?v=' . time()) }}">
     <link rel="icon"
         href="https://i0.wp.com/flyinghighexpress.com/wp-content/uploads/2019/02/cropped-flyinghighenergyexpress-logoicon.png?fit=32%2C32&ssl=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
